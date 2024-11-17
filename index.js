8f20ddf30b89b5b807c8c6d1a76fa949696bea3b
haukientran <70125147+haukientran@users.noreply.github.com>
2024-11-18 00:58:44 +0700

    udate webthietke

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/kienit', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
-  
-  res.send('Hello World!43')
+  res.send('Hello World!3')
 })
 
 app.listen(port, () => {
